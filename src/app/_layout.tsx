d6d5f1f48bf5a38e36cfd52257aae6f3e85d9c87
S-hossain09 <sahilhossain1429@gmail.com>
2026-08-17 23:05:49 +0600

    Implement create folder, import file, list folder, delete file, rename file, move file logic from expo-file-system api

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,15 @@
 import "@/global.css";
+import { ensureVaultDir } from "@/lib/fileStorage";
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { Stack } from "expo-router";
+import { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
+  useEffect(() => {
+    ensureVaultDir();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <BottomSheetModalProvider>
