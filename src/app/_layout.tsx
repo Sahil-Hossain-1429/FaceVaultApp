326779063a692a8f1f7dcb12b1b8bda65d55c443
S-hossain09 <sahilhossain1429@gmail.com>
2026-08-17 15:32:43 +0600

    Implement Add button ui on the home screen, and give tab bar icons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,5 +2,9 @@ import "@/global.css";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
